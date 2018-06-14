66ee1034629b141bf0882d0e31219b41551cd76d
Noel <noel@2bedigital.com>
2018-06-14 11:44:19 +0100

    added overrides

---
+++ app/overrides/products_edit.rb
@@ -0,0 +1,14 @@
+Deface::Override.new(virtual_path: "spree/admin/products/_form",
+                     insert_after: "[data-hook='admin_product_form_promotionable']",
+                     name: "products_retail_only",
+                     text: "<div data-hook='admin_product_form_retail'>
+	                            <%= f.field_container :retail_only do %>
+	                                    <%= f.label :retail_only do %>
+	                                      <%= f.check_box :retail_only %> <%= Spree.t(:retail_only) %>
+	                                    <% end %>
+	                            <% end %>
+                            </div>")
+
+
+
+
---
+++ app/overrides/search_base.rb
@@ -0,0 +1,13 @@
+Spree::Core::Search::Base.class_eval do
+  alias_method :orig_get_base_scope,  :get_base_scope unless method_defined? :orig_get_base_scope
+  def get_base_scope
+    base_scope = orig_get_base_scope
+    return base_scope if current_user and current_user.has_spree_role? :admin
+    if current_user and current_user.has_spree_role? :retail
+      base_scope = base_scope.where("#{Spree::Product.quoted_table_name}.retail_only = ?", true).references("#{Spree::Product.quoted_table_name}")
+    else
+      base_scope = base_scope.where("#{Spree::Product.quoted_table_name}.retail_only != ?", true).references("#{Spree::Product.quoted_table_name}")
+    end
+    base_scope
+  end
+end

f1e0fb984c87f3b1f44dedee64655a1871810fa7
Noel <noel@2bedigital.com>
2019-01-21 13:17:36 +0000

    bug in show taxons products

--- app/helpers/taxons_helper_decorator.rb
+++ app/helpers/taxons_helper_decorator.rb
@@ -1,7 +1,7 @@
 Spree::TaxonsHelper.class_eval do
   alias_method :orig_taxon_preview,  :taxon_preview unless method_defined? :orig_taxon_preview
   def taxon_preview(taxon, max=4)
-    products = taxon.active_products
+    products = taxon.active_products.uniq
     return products.limit(max).uniq if spree_current_user and spree_current_user.has_spree_role? :admin
     if spree_current_user and spree_current_user.has_spree_role? :retail
       products = products.where("#{Spree::Product.quoted_table_name}.retail_only = ?", true).references("#{Spree::Product.quoted_table_name}")
